b2b71c053be54bd69f0b4070fde4ad61b5ac6d67
Mark Cameron <mark.cameron@devfactory.ch>
2018-04-15 15:01:15 +0200

    Added predictor migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,32 +4,38 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+class CreateUsersTable extends Migration {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('users', function (Blueprint $table) {
+      $table->increments('id');
+      $table->timestamps();
+      $table->datetime('last_seen')->nullable();
+      $table->boolean('is_admin')->default(FALSE);
+      $table->boolean('can_predict')->default(TRUE);
+      $table->string('nickname');
+      $table->string('last_name');
+      $table->string('first_name');
+      $table->string('email')->unique();
+      $table->integer('active')->default(0);
+      $table->string('password');
+      $table->string('activation_code');
+      $table->rememberToken();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('users');
+  }
+
 }
