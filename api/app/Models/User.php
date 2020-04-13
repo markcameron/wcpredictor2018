c2935f89d36883b449a1801de7a9e3d011bda45b
Mark Cameron <mark.cameron@devfactory.ch>
2020-04-13 11:26:38 +0200

    Updated PassportSocialite use statement to match updated package

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Anand\Laravel\PassportSocialite\User\UserSocialAccount;
+use Schedula\Laravel\PassportSocialite\User\UserSocialAccount;
 
 class User extends Authenticatable implements UserSocialAccount {
 
